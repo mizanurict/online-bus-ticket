dfe0df8194f4234ec513ec5757d242fc97c7eec9
Mizanur_Rahman <mizanurictiu@gmail.com>
2024-02-19 13:18:20 +0600

    added button loop access

--- scripts/script.js
+++ scripts/script.js
@@ -3,34 +3,38 @@ document.getElementById("buy-ticket").addEventListener('click', function(){
     showParibahanPart.scrollIntoView();
 })
 
-let count=1;
-let totalPrice =0;
-document.getElementById('a1').addEventListener('click', function(e){
-    e.target.style.backgroundColor = "orange";
-const totalSeat = parseInt(document.getElementById("total-seat").innerText);
-const newTotalSeat = totalSeat-1;
-document.getElementById("total-seat").innerText = newTotalSeat;
-document.getElementById("booking-seat").innerText = count;
-count++;
+let count = 1;
+let totalPrice = 0;
 
-const toAddSeat = document.getElementById("to-add-seat");
-const div =document.createElement("div");
-div.classList.add("new-class");
-const p= document.createElement("p");
-p.innerText=document.getElementById('a1').innerText;
-const p2 = document.createElement("p");
-p2.innerText = "Economoy";
-const p3 = document.createElement("p");
-p3.innerText = "550";
+const allBtn = document.getElementsByClassName("seat-btn");
+for (const btn of allBtn){
+    btn.addEventListener('click', function(e){
+      e.target.style.backgroundColor = "orange";
 
-div.appendChild(p);
-div.appendChild(p2);
-div.appendChild(p3);
+      const totalSeat = parseInt(document.getElementById("total-seat").innerText);
+      const newTotalSeat = totalSeat - 1;
+      document.getElementById("total-seat").innerText = newTotalSeat;
+      document.getElementById("booking-seat").innerText = count;
+      count++;
+      const btnClass = btn.innerText;
+    
+      const toAddSeat = document.getElementById("to-add-seat");
+      const div = document.createElement("div");
+      div.classList.add("new-class");
+      const p = document.createElement("p");
+      p.innerText = btnClass;
+      const p2 = document.createElement("p");
+      p2.innerText = "Economoy";
+      const p3 = document.createElement("p");
+      p3.innerText = "550";
 
-toAddSeat.appendChild(div);
+      div.appendChild(p);
+      div.appendChild(p2);
+      div.appendChild(p3);
 
+      toAddSeat.appendChild(div);
 
-totalPrice =totalPrice+550;
-document.getElementById("total-price").innerText=totalPrice;
-
-})
+      totalPrice = totalPrice + 550;
+      document.getElementById("total-price").innerText = totalPrice;
+    })
+}
